4414e53a18bc424bba0995d7acd9d54eba802e89
Craig McCartney <craig.a.mccartney@gmail.com>
2020-05-04 16:50:51 +0000

    Add project files.

---
+++ ShoppingApi/Controllers/ShoppingListController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using ShoppingApi.Models;
+
+namespace ShoppingApi.Controllers
+{
+    public class ShoppingListController : ControllerBase
+    {
+        [HttpGet("shoppinglist")]
+        public async Task<ActionResult> GetFullShoppingList()
+        {
+            var response = new GetShoppingListResponse();
+
+            return Ok(response);
+        }
+    }
+}
